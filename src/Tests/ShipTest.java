2e6449b539ea8845a9b1ab04a2c160f48026ba4d
David Olsen <davidols@buffalo.edu>
2017-08-09 17:44:47 -0400

    Class names and packages defined.

---
+++ src/Tests/ShipTest.java
@@ -0,0 +1,17 @@
+package Tests;
+
+import java.util.*;
+
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+public class ShipTest {
+
+    @Test
+    public void doNothing(){
+        assertEquals("hi", "hi");
+        assertFalse(false);
+    }
+
+}
--- src/game/Main.java
+++ src/game/Main.java
@@ -1,8 +1,9 @@
-package root;
+package game;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        System.out.println("Hello world!");
     }
 }
